7f297d1bc10e76846dce1eff74900a98dc0e499a
yjmantilla <yjmantilla@gmail.com>
2018-02-23 11:20:32 -0500

    se agrega el ejercicio 2

---
+++ ejercicio_2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+	programa que dice si un numero es par o impar
+*/
+int main()
+{
+	int number;
+	cout<< "Inserte el numero"<<endl;
+	cin>> number; // Se pide un numero por consola
+
+	cout<<"El numero que escribio es:"<<number<<endl;
+
+	if(number%2==0){cout<<"El numero"<<number<<"es par"<<endl;}
+	else{cout<<"El numero"<<number<<"es impar"<<endl;}
+
+}
