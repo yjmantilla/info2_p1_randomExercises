bfd14bcf9d51d9d948d5fd261dd361968e04658e
yjmantilla <yjmantilla@gmail.com>
2018-02-23 11:56:17 -0500

    primera version ejercicio 7, no compila

---
+++ ejercicio_7/main.cpp
@@ -0,0 +1,27 @@
+
+/*
+Ejercicio 7. Escriba un programa que pida un número N e imprima en pantalla la suma de todos
+los números entre 0 y N (incluyéndose el mismo).
+Ej: si se ingresa 5: 1+2+3+4+5=15, por lo que se debe imprimir:
+La sumatoria desde 0 hasta 5 es: 15
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int N;
+	int sum;	
+	
+	//inicializacion
+	N=0; sum=0;	
+	cout<<"Inserte el numero N hasta el que desea hacer la suma de los numeros naturales"<<endl;
+	
+	for(int i=1,i<=N,i++)
+		{
+			sum=sum+i;
+		}	
+	cout<<"La sumatorio desde 1 hasta "<<N<<" es "<<sum<<endl;
+	return sum;		
+}
+
