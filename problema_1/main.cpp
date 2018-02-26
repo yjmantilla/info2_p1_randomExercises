7d19682c081ac9877793187e4aaa740835ce6d1e
yjmantilla <yjmantilla@gmail.com>
2018-02-25 19:49:56 -0500

    se anyaden comentarios al problema 1

--- problema_1/main.cpp
+++ problema_1/main.cpp
@@ -16,9 +16,13 @@ int main()
 {
 
 
-    char letra=0;
-    int vocal_may=0;
-    int vocal_min=0;
+    char letra=0; //letra a introducir por el usuario
+    int vocal_may=0; //variable para saber si letra es una vocal mayuscula
+    int vocal_min=0; //variable para saber si letra es una vocal minuscula
+    int consonante_may=0;//variable para saber si letra es una consonante mayuscula
+    int consonante_min=0;//variable para saber si letra es una consonante minuscula
+
+    //ciclo while permite que el usuario introduzca letras indefinidamente hasta que el ponga 0 para terminar el programa
 
     while(1)
     {
@@ -26,16 +30,28 @@ int main()
     cin >> letra;
     cout<<endl<<endl;
 
+    //chequeamos si letra es vocal:
     vocal_min=(letra =='a' ||letra =='e' || letra =='i' || letra =='o' ||letra =='u');
     vocal_may=(letra =='A' ||letra =='E' || letra =='I' || letra =='O' ||letra =='U');
 
     if (vocal_may || vocal_min){cout<<letra<<" es una vocal"<< endl<< endl;}
-    else
-    {
-        if((65<=letra && letra<= 90) || (97<=letra && letra<= 122)){cout<<letra<<" es una consonante"<< endl<< endl;}
-        else{cout<<letra<<" no es una letra"<< endl<< endl;}
-    }
-    if (letra==48){break;}
+        else //si letra no es vocal probamos si es consonante
+            {
+                //para saber si es consonante (ya sabiendo que no es vocal) se debe cumplir que
+                //65<=letra<=90 o  97<=letra<=122 por el codigo ascii
+
+                consonante_may=(65<=letra && letra<= 90);
+                consonante_min=(97<=letra && letra<= 122);
+
+                if(consonante_may|| consonante_min){cout<<letra<<" es una consonante"<< endl<< endl;}
+
+                    else
+                        //si letra no cumple ninguna de las anteriores entonces no es una letra
+                        {cout<<letra<<" no es una letra"<< endl<< endl;}
+            }
+    //si el usuario introduce cero finaliza el programa (0 en ASCII es 48)
+    if (letra==48){cout<<endl<<"Hasta Luego!"<<endl;break;}
+
     }
     return 0;
 }
